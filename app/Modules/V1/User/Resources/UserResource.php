1c7a8992074782593a0dbfe04ec45d5a678e2d82
ihorbayrak <bayrak.igor563@gmail.com>
2023-11-28 05:20:37 +0200

    Create update profile form request, profile api resources and refactor user api resource

--- app/Modules/V1/User/Resources/UserResource.php
+++ app/Modules/V1/User/Resources/UserResource.php
@@ -11,8 +11,8 @@ class UserResource extends JsonResource
     {
         return [
             'id' => $this->resource->id,
-            'name' => $this->resource->name,
-            'email' => $this->resource->email
+            'name' => $this->resource->name ?? $this->resource->user->name, // If resource = Profile than we get user's name through Profile,
+            'email' => $this->resource->email ?? $this->resource->user->email, // If resource = Profile than we get email through Profile
         ];
     }
 }
